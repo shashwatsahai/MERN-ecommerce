1a716dd6237a4b181f700ed9b8589b21f748b84f
shashwatsahai <ssahai97@gmail.com>
2020-08-01 16:23:21 +0530

    changes for auth routes

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,12 +1,83 @@
-exports.signup = (req, res) => {
-  console.log("REQ",req.body)
-  res.json({
-    message: "signup route works!"
+const { validationResult } = require("express-validator");
+const User = require('../models/user');
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
+
+var signup = (req, res) => {
+  const errors = validationResult(req)
+  // console.log(errors);
+  if(!errors.isEmpty()){
+    return res.status(422).json({
+      error: errors.array()[0].msg
+    })
+  }
+  const user = new User(req.body);
+
+  user.save((err, user) => {
+    if(err){
+      console.log("ERROR", err);
+      return res.status(400).json({
+        error: "NOT able to save user to db"+err
+      })
+    }
+    console.log('Saved user')
+    res.json({
+      name: user.name,
+      email: user.email,
+      id: user._id
+    });
   });
 };
 
-exports.signout = (req, res) => {
+var signout = (req, res) => {
+  res.clearCookie('token')
   res.json({
-    message: "User signout"
+    message: "User signed out successfully"
   });
 };
+
+var signin = (req, res) => {
+  const errors = validationResult(req)
+  // console.log(errors);
+  if(!errors.isEmpty()){
+    return res.status(422).json({
+      error: errors.array()[0].msg
+    })
+  } 
+  const {email, password} = req.body;
+
+  User.findOne({email: email},(err, user) => {
+    console.log(err)
+    if(err || !user){
+      return res.status(400).json({
+        error: "USER email does not exist"
+      })
+    }
+
+
+    if(!user.authenticate(password)){
+      return res.status(400).json({
+        error: "Incorrect email or password"
+      })
+    }
+
+    const token = jwt.sign({_id: user._id}, process.env.PRIVATE_KEY);
+    res.cookie("token", token, {expire: new Date() + 99999});
+    const {_id, name, email, role} = user;
+    return res.json({token, user:{_id, name, email, role}});
+  })
+
+
+};
+
+var isSignedIn = expressJwt({
+  secret: process.env.PRIVATE_KEY,
+  userProperty: "auth"
+})
+
+module.exports = {
+  signup,
+  signout,
+  signin,
+  isSignedIn
+}
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,8 +1,27 @@
 var express = require("express");
 var router = express.Router();
-const { signout, signup } = require("../controllers/auth");
+const {check, validationResult} = require('express-validator');
+const { signout, signup, signin, isSignedIn } = require("../controllers/auth");
 
-router.post("/signup", signup);
+router.post("/signup",
+[
+    check('name', 'name should be at least 3 characters').isLength({min:3}),
+    check("email","email is required").isEmail(),
+    check("password", "password at least 3 char").isLength({min:3})
+],
+ signup);
 router.get("/signout", signout);
+
+router.post("/signin",
+[
+    check('name', 'name should be at least 3 characters').isLength({min:3}),
+    check("email","email is required").isEmail(),
+    check("password", "password is required").isLength({min:1})
+],
+ signin);
+
+router.get("/testroute", isSignedIn, (req, res) => {
+    res.send("Passed a protected route")
+})
 
 module.exports = router;
