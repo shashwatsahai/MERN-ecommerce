d0351b9ced004247756e48ede916c361885c3ba8
Shashwat Sahai <ssahai97@gmail.com>
2020-10-23 18:14:11 +0530

    changes

---
+++ projfrontend/src/user/Signup.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import Base from "../core/Base";
+import { Link } from "react-router-dom";
+import { signup } from "../auth/helper";
+
+const Signup = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+  });
+
+  const { name, email, password, error, success } = values;
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false });
+    signup({ name, email, password }).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: true, success: false });
+      } else {
+        setValues({
+          ...values,
+          name: "",
+          email: "",
+          password: "",
+          error: "",
+          success: true,
+        });
+      }
+    });
+  };
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const signUpForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Name</label>
+              <input
+                className="form-control"
+                onChange={handleChange("name")}
+                type="text"
+              />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Email</label>
+              <input
+                className="form-control"
+                onChange={handleChange("email")}
+                type="email"
+              />
+            </div>
+
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input
+                className="form-control"
+                onChange={handleChange("password")}
+                type="password"
+              />
+            </div>
+            <button className="btn btn-success btn-block" onClick={onSubmit}>
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <Base title="Sign up page" description="A page for user to sign up!">
+      {signUpForm()}
+      <p className="text-white">{JSON.stringify(values)}</p>
+    </Base>
+  );
+};
+
+export default Signup;
